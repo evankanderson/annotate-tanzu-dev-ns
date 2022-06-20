b7dc10a73b6e29fea4fa305aa1664aa426207e1a
Evan Anderson <evana@vmware.com>
2022-06-20 00:49:29 -0700

    Log request body, further debugging

--- func.py
+++ func.py
@@ -1,6 +1,8 @@
 from typing import Any
+from flask import Request
 import sys
 
-def decorate_namespace(req: Any) -> dict[str,Any]:
-    print("Req:", req, file=sys.stderr)
+def decorate_namespace(req: Request) -> dict[str,Any]:
+    data = req.get_json()
+    print("Req:", data, file=sys.stderr)
     return {}
