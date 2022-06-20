c80833bf50e441d514f0476fb86da7a7c9aa0020
Evan Anderson <evana@vmware.com>
2022-06-20 01:03:26 -0700

    Create the first two objects per https://docs.vmware.com/en/VMware-Tanzu-Application-Platform/1.1/tap/GUID-install-components.html#setup

--- func.py
+++ func.py
@@ -2,7 +2,45 @@ from typing import Any
 from flask import Request
 import sys
 
+def reg_creds(namespace: str) -> dict[str, Any]:
+    return {"apiVersion": "v1",
+            "kind": "Secret",
+            "metadata": {
+                "name": "tap-registry",
+                "namespace": namespace,
+                "annotations": {
+                    "secretgen.carvel.dev/image-pull-secret": "",
+                },
+            },
+            "type": "kubernetes.io/dockerconfigjson",
+            "data": {
+                ".dockerconfigjson": "e30K",
+            },
+           }
+
+def sa(namespace: str) -> dict[str, Any]:
+    return {"apiVersion": "v1",
+            "kind": "ServiceAccount",
+            "metadata": {
+                "name": "default",
+                "namespace": namespace,
+            },
+            "secrets": [
+                {"name": "registry-credentials"},
+            ],
+            "imagePullSecrets": [
+                {"name": "registry-credentials"},
+                {"name": "tap-registry"},
+            ],
+           }
+
 def decorate_namespace(req: Request) -> dict[str,Any]:
     data = req.get_json()
     print("Req:", data, file=sys.stderr)
-    return {}
+    ns = data["object"]["metadata"]["name"]
+    return dict(labels={},
+                annotations={},
+                status=None,
+                attachments=[reg_creds(ns),sa(ns)],
+               )
+                
